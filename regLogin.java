815b5e4396b04b6343b6d833e2f405bc2e09af8e
unknown <dsinghal1265@gmail.com>
2023-10-13 12:02:21 +0530

    this is first

---
+++ regLogin.java
@@ -0,0 +1,31 @@
+
+//Importing files
+import java.io.FileWriter;
+import java.io.IOException;
+
+//Class User_Info
+class regLogin extends user_info {
+    // Variable Money
+    int money;
+
+    // Constructor to create file username.txt and to write initial values in it
+    regLogin(String name, String username, String password, String number, String transaction, String FilePath) {
+        super(name, username, password, number);
+        this.money = 0;
+        try {
+            FileWriter fwrite = new FileWriter(FilePath + this.getUsername() + ".txt", true);
+            fwrite.write(this.money + "\r\n" + this.getName() + "\r\n" + this.getUsername() + "\r\n"
+                    + this.getPassword() + "\r\n" + this.getNumber() + "\r\n" + transaction + "\r\n");
+            fwrite.close();
+            // System.out.println("Content is successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("Unexpected error occurred");
+            e.printStackTrace();
+        }
+    }
+
+    // Message that will be shown on successful registration
+    void message() {
+        System.out.println("USER GENERATED SUCCESFULLY !");
+    }
+}
---
+++ user_info.java
@@ -0,0 +1,50 @@
+
+//Class for register/login
+class user_info {
+    // Variables to store different stuff
+    private String username;
+    private String password;
+    private String Name;
+    private String number;
+
+    // Constructor to initialise variables that are private
+    user_info(String name, String username, String password, String number) {
+        setName(name);
+        setUsername(username);
+        setPassword(password);
+        setNumber(number);
+    }
+
+    // Getters and setters as they are private
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+}
